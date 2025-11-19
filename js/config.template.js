5c30209ff0759b61f77886ba98031acdc62c9fd5
okrbb <123800197+okrbb@users.noreply.github.com>
2025-11-19 18:17:06 +0100

    Add files via upload

---
+++ js/config.template.js
@@ -0,0 +1,15 @@
+// js/config.template.js
+export const firebaseConfig = {
+    apiKey: "FIREBASE_API_KEY_PLACEHOLDER",
+    authDomain: "FIREBASE_AUTH_DOMAIN_PLACEHOLDER",
+    projectId: "FIREBASE_PROJECT_ID_PLACEHOLDER",
+    storageBucket: "FIREBASE_STORAGE_BUCKET_PLACEHOLDER",
+    messagingSenderId: "FIREBASE_MESSAGING_SENDER_ID_PLACEHOLDER",
+    appId: "FIREBASE_APP_ID_PLACEHOLDER"
+};
+
+export const APP_CONSTANTS = {
+    TOAST_DURATION: 3000,
+    SEARCH_DEBOUNCE_MS: 300,
+    DEFAULT_AVATAR: '--'
+};
